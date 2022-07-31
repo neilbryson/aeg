82bb553def283684951e616739e3e5e0f0d9b6e7
Neil Bryson Cargamento <neilbrysonmc@gmail.com>
2022-07-31 13:52:02 +0800

    fix: add missing key to image loop

--- src/components/ImageGrid.tsx
+++ src/components/ImageGrid.tsx
@@ -10,7 +10,7 @@ export const ImageGrid = ({ images }: ImageGridProps) => {
   return (
     <div className="grid grid-rows-2 grid-cols-2 gap-4 items-center justify-items-center">
       {images.map(([src, largeSrc], index) => {
-        return <Image className={index === 0 ? `row-span-2` : ''} src={src} largeSrc={largeSrc} />;
+        return <Image key={src} className={index === 0 ? `row-span-2` : ''} src={src} largeSrc={largeSrc} />;
       })}
     </div>
   );
