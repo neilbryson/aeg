af8c8b25b0148a231f95d30a543c0105e3c584c0
Neil Bryson Cargamento <neilbrysonmc@gmail.com>
2022-07-31 13:06:59 +0800

    feat: create PreviewCard component

---
+++ src/components/PreviewCard.tsx
@@ -0,0 +1,22 @@
+import type { HTMLAttributes, ReactNode } from 'react';
+
+import { Image } from './Image';
+import { Link } from './Link';
+
+export type PreviewCardProps = {
+  imgSrc: string;
+  imgLargeSrc?: string;
+  title: string;
+  children: ReactNode;
+} & HTMLAttributes<HTMLElement>;
+
+export const PreviewCard = ({ children, className, imgLargeSrc, imgSrc, title }: PreviewCardProps) => {
+  return (
+    <section className={`w-full md:w-96 ${className}`}>
+      <Image className="mb-6" src={imgSrc} largeSrc={imgLargeSrc} isHighlighted />
+      <h3 className="font-semibold text-lg mb-3">{title}</h3>
+      <p className="font-light text-aeg-gray mb-6">{children}</p>
+      <Link href="#">Read more</Link>
+    </section>
+  );
+};
