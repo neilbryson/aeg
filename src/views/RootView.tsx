960f8273b19ffda2e69b0277cbd45c80b70b1895
Neil Bryson Cargamento <neilbrysonmc@gmail.com>
2022-07-31 09:53:37 +0800

    chore: remove unnecessary code from template

--- src/views/RootView.tsx
+++ src/views/RootView.tsx
@@ -1,22 +1,15 @@
-import { Provider } from 'react-redux';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-import { useTheme } from '../hooks/useTheme';
-import store from '../store/configureStore';
 import { NotFoundView } from './NotFoundView';
-import { SampleView } from './SampleView';
 
 export const RootView = () => {
-  useTheme();
-
   return (
-    <Provider store={store}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<SampleView />} />
-          <Route path="*" element={<NotFoundView />} />
-        </Routes>
-      </BrowserRouter>
-    </Provider>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<div>Hello</div>} />
+
+        <Route path="*" element={<NotFoundView />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
