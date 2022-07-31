96d76158ce46d312413f6b459e1f203c28898168
Neil Bryson Cargamento <neilbrysonmc@gmail.com>
2022-07-31 13:49:40 +0800

    fix: margin and padding updates

--- src/components/HeadingText.tsx
+++ src/components/HeadingText.tsx
@@ -9,7 +9,9 @@ export type HeadingTextProps = {
 export const HeadingText = ({ className, title, children, isSubText = false }: HeadingTextProps) => {
   return (
     <section className={className}>
-      <h1 className={`${isSubText ? 'font-medium text-sm text-aeg-red' : 'text-2xl'} mb-4`}>{title}</h1>
+      <h1 className={`${isSubText ? 'font-medium text-sm text-aeg-red' : 'text-2xl font-light text-white'} mb-4`}>
+        {title}
+      </h1>
       {!isSubText && <div className="w-full h-[2px] bg-aeg-gray mb-4" />}
       <p className={`leading-8 ${isSubText ? 'font-bold text-white' : 'font-light'}`}>{children}</p>
     </section>
