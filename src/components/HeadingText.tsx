e6c20ab15fe4eedd86c8becbaaff8c8fbe343d96
Neil Bryson Cargamento <neilbrysonmc@gmail.com>
2022-07-31 12:20:03 +0800

    feat(heading): allow extending of classes

--- src/components/HeadingText.tsx
+++ src/components/HeadingText.tsx
@@ -1,15 +1,15 @@
-import { ReactNode } from 'react';
+import { HTMLAttributes, ReactNode } from 'react';
 
 export type HeadingTextProps = {
   title: string;
   children: ReactNode;
   isSubText?: boolean;
-};
+} & HTMLAttributes<HTMLElement>;
 
-export const HeadingText = ({ title, children, isSubText = false }: HeadingTextProps) => {
+export const HeadingText = ({ className, title, children, isSubText = false }: HeadingTextProps) => {
   return (
-    <section>
-      <h1 className={`${isSubText ? 'font-medium text-sm text-aeg-red' : 'text-2xl'} mb-2`}>{title}</h1>
+    <section className={className}>
+      <h1 className={`${isSubText ? 'font-medium text-sm text-aeg-red' : 'text-2xl'} mb-4`}>{title}</h1>
       {!isSubText && <div className="w-full h-[2px] bg-aeg-gray mb-4" />}
       <p className={`${isSubText ? 'font-bold' : 'font-light'}`}>{children}</p>
     </section>
