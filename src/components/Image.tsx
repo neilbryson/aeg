b813a91555d4c1839eb1956b16207e8345b63822
Neil Bryson Cargamento <neilbrysonmc@gmail.com>
2022-07-31 11:04:36 +0800

    feat: implement image with opening modal

---
+++ src/components/Image.tsx
@@ -0,0 +1,63 @@
+import { type ImgHTMLAttributes, Fragment, useEffect, useRef, useState } from 'react';
+import ReactDOM from 'react-dom';
+
+export type ImageProps = {
+  // Adds a border at the bottom of the image
+  isHighlighted?: boolean;
+  // The image to be displayed in the modal. If not supplied, the file from `src` will be used.
+  largeSrc?: string;
+} & ImgHTMLAttributes<HTMLImageElement>;
+
+export const Image = ({ alt, isHighlighted = false, largeSrc, src, ...props }: ImageProps) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const imageRef = useRef<HTMLImageElement>(null);
+  const modalRef = useRef<HTMLElement>(null);
+  const portalRoot = document.getElementById('image-portal');
+
+  useEffect(() => {
+    const modal = modalRef.current;
+    if (!modal) return;
+
+    function closeModal(e: Event): void {
+      if (!(e.target instanceof Node)) return;
+      // Prevent modal from closing when clicking on the enlarged image
+      if (e.target === imageRef.current) return;
+      // Allow closing modal with ESC for KeyboardEvent
+      if (e instanceof KeyboardEvent && e.key !== 'Escape') return;
+      setIsOpen(false);
+    }
+
+    if (isOpen) {
+      modal.addEventListener('click', closeModal);
+      window.addEventListener('keydown', closeModal);
+    }
+
+    return () => {
+      if (isOpen) {
+        modal.removeEventListener('click', closeModal);
+        window.removeEventListener('keydown', closeModal);
+      }
+    };
+  }, [isOpen]);
+
+  function onClickImage(): void {
+    setIsOpen(true);
+  }
+
+  function renderModalContent() {
+    return (
+      <aside className="w-screen h-screen" ref={modalRef}>
+        <div className="bg-translucent w-full h-full flex justify-center items-center">
+          <img src={largeSrc ?? src} alt={alt} ref={imageRef} />
+        </div>
+      </aside>
+    );
+  }
+
+  return (
+    <Fragment>
+      <img alt={alt} className="cursor-pointer" onClick={onClickImage} src={src} {...props} />
+      {portalRoot && isOpen && ReactDOM.createPortal(renderModalContent(), portalRoot)}
+    </Fragment>
+  );
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -13,8 +13,20 @@ module.exports = {
       '2xl': '2400px',
     },
     fontFamily: {
-      sans: ['Work Sans', 'sans-serif'],
+      sans: ['OpenSans', 'sans-serif'],
       mono: ['ui-monospace', 'monospace'],
+    },
+    extend: {
+      colors: {
+        'aeg-black': '#0e1414',
+        'aeg-white': '#fdfdfd',
+        'aeg-gray': '#b9b9b9',
+        'aeg-red': '#ca142f',
+        translucent: '#e5e7eb87'
+      },
+      fontFamily: {
+        sansCondensed: ['OpenSans SemiCondensed', 'sans-serif'],
+      },
     },
   },
   plugins: [],
