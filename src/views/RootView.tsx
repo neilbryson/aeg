ff0bd9a73f468eced75766e8b5df841b8dfae138
Neil Bryson Cargamento <neilbrysonmc@gmail.com>
2022-07-31 13:38:38 +0800

    feat: capture all anchor element clicks

--- src/views/RootView.tsx
+++ src/views/RootView.tsx
@@ -1,9 +1,25 @@
+import { useEffect } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import { MainView } from './MainView';
 import { NotFoundView } from './NotFoundView';
 
 export const RootView = () => {
+  useEffect(() => {
+    // Capture all anchor clicks!
+    function handleAnchorClick(e: MouseEvent): void {
+      if (!(e.target instanceof HTMLAnchorElement)) return;
+      // eslint-disable-next-line no-console
+      console.log('Anchor element clicked', e.target);
+    }
+
+    window.addEventListener('click', handleAnchorClick);
+
+    return () => {
+      window.removeEventListener('click', handleAnchorClick);
+    };
+  }, []);
+
   return (
     <BrowserRouter basename={process.env.BASE_PATH || '/'}>
       <Routes>
